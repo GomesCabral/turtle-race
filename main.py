2365f3b33e6d3fdc26128a9b096b694a98d2f95a
gomes cabral <phgcabral@hotmail.com>
2023-06-09 17:48:51 +0200

    start race

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import random
 
 
 my_screen = Screen()
-
+is_race_on = False
+all_turtles = []
 
 #SCREEN SIZE
 my_screen.setup(width=500, height=400)
@@ -20,9 +21,25 @@ for turtle in range(0, 6):
     heni.penup()
     heni.color(random.choice(colors))
     heni.goto(x=-240, y=y_positions[turtle])
+    all_turtles.append(heni)
+
+#CHECK IF THE USER COLOR EXIST
+if user_bet:
+    is_race_on = True
 
 
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 210:
+            is_race_on = False
+            winner = turtle.pencolor()
+            if winner == user_bet:
+                print(f"You've Won! {winner} turtle is the winner!")
+            else:
+              print(f"You've Lost! {winner} turtle is the winner!")  
 
+        random_distance = random.randint(0, 10)
+        turtle.forward(random_distance)
 
 
 
